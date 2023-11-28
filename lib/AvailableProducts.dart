45e085d610d0dc3cc304ebc75f6208b299d87012
OmarHafez1 <omarahmedhappy@gmail.com>
2023-11-28 02:48:19 +0200

    Finished the Home Page (code is not refactored yet)

---
+++ lib/AvailableProducts.dart
@@ -0,0 +1,70 @@
+import 'package:shopping_app/Product.dart';
+
+List<Product> availableProducts = [
+  Product(
+    title: "Men's Nike Shoes",
+    price: 44.52,
+    sizes: [37, 38, 40, 42, 43],
+    imagePath: "assets/images/0.png",
+  ),
+  Product(
+    title: "Men's Beta Shoes",
+    price: 51.44,
+    sizes: [38, 39, 42, 43],
+    imagePath: "assets/images/1.png",
+  ),
+  Product(
+    title: "Addidas Shoes",
+    price: 38.99,
+    sizes: [37, 40, 42, 43, 44, 45],
+    imagePath: "assets/images/2.png",
+  ),
+  Product(
+    title: "Men's Beta Shoes",
+    price: 61.34,
+    sizes: [42, 43, 44, 45],
+    imagePath: "assets/images/3.png",
+  ),
+  Product(
+    title: "Nike Shoes",
+    price: 31.99,
+    sizes: [37, 38, 40, 42],
+    imagePath: "assets/images/4.png",
+  ),
+  Product(
+    title: "Beta Shoes",
+    price: 40.59,
+    sizes: [37, 38, 39, 40, 42, 43, 44],
+    imagePath: "assets/images/5.png",
+  ),
+  Product(
+    title: "Men's Nike Shoes",
+    price: 59.41,
+    sizes: [40, 42, 43],
+    imagePath: "assets/images/6.png",
+  ),
+  Product(
+    title: "Women's Addidas Shoes",
+    price: 39.19,
+    sizes: [37, 38, 40],
+    imagePath: "assets/images/7.png",
+  ),
+  Product(
+    title: "Men's Beta Shoes",
+    price: 44.25,
+    sizes: [37, 42, 43],
+    imagePath: "assets/images/8.png",
+  ),
+  Product(
+    title: "Addidas Shoes",
+    price: 41.99,
+    sizes: [38, 41, 42, 43],
+    imagePath: "assets/images/9.png",
+  ),
+  Product(
+    title: "Women's Beta Shoes",
+    price: 43.29,
+    sizes: [37, 38, 39, 40],
+    imagePath: "assets/images/10.png",
+  ),
+];
---
+++ lib/Product.dart
@@ -0,0 +1,12 @@
+class Product {
+  String title;
+  double price;
+  List<num> sizes;
+  String imagePath;
+  Product({
+    required this.title,
+    required this.price,
+    required this.sizes,
+    required this.imagePath,
+  });
+}
